db0dd48f7af994252cfc5d51c32ab8c63f7d227b
Zac <zacaree@gmail.com>
2018-10-11 15:33:26 -0500

    Built the Redux function createStore from scratch

--- 006 Store Methods_getState()_dispatch()_subscribe()/script.js
+++ 006 Store Methods_getState()_dispatch()_subscribe()/script.js
@@ -1,39 +1,34 @@
-
 // Counter is the reducer that manages the state updates.
 const counter = (state = 0, action) => {
   switch (action.type) {
-    case 'INCREMENT':
+    case "INCREMENT":
       return state + 1;
-    case 'DECREMENT':
+    case "DECREMENT":
       return state - 1;
     default:
       return state;
   }
 };
 
-
 // The store has 3 important methods
 const { createStore } = Redux;
 const store = createStore(counter);
 
-
 // getState()
-// The first method of the store is called getState 
+// The first method of the store is called getState
 // and it retrieves the current state of the Redux store.
-console.log(store.getState())
-
-
-// dispatch()
-// The second and most commonly used store method is called dispatch 
-// and it lets you dispatch actions to change the state of your application.
-store.dispatch({ type: 'INCREMENT' });
 console.log(store.getState());
 
+// dispatch()
+// The second and most commonly used store method is called dispatch
+// and it lets you dispatch actions to change the state of your application.
+store.dispatch({ type: "INCREMENT" });
+console.log(store.getState());
 
 // subscribe()
 // The third Redux store method is called subscribe.
-// It lets you register a callback that the Redux store will call any time an action 
-// has been dispatched so that you can update the UI of 
+// It lets you register a callback that the Redux store will call any time an action
+// has been dispatched so that you can update the UI of
 // your application to reflect the current application state.
 // See all three methods working together ↓
 const render = () => {
@@ -43,6 +38,6 @@ const render = () => {
 store.subscribe(render);
 render();
 
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
+document.addEventListener("click", () => {
+  store.dispatch({ type: "INCREMENT" });
 });
