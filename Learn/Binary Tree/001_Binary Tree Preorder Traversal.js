fe0268b6a612590c8f7b513b9c67a4e62898c67c
zacaree <zacharem@gmail.com>
2019-11-10 22:04:57 -0600

    Doing some reading on data structures

---
+++ Learn/Binary Tree/001_Binary Tree Preorder Traversal.js
@@ -0,0 +1,34 @@
+/*
+  Given a binary tree, return the preorder traversal of its nodes' values.
+  Example ↓
+
+  Input: [1,null,2,3]
+
+  1
+    \
+    2
+    /
+  3
+
+  Output: [1,2,3]
+
+  Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+//////////////////////
+
+/*
+  Definition for a binary tree node.
+
+  function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+
+  @param {TreeNode} root
+  @return {number[]}
+*/
+
+var preorderTraversal = function(root) {
+  //
+};
